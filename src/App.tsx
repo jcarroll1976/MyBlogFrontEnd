02897258f6b5888232bb1f5a7288ccf38a74ed01
Joshua Carroll <carrolljosh1976@gmail.com>
2023-11-27 21:59:59 -0500

    implemented React Router and created routes for Homepage and Signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import {Navigate,Route,BrowserRouter as Router, Routes} from "react-router-dom";
 import './App.css';
 import Home from "./components/Home.tsx";
+import SignUp from './components/SignUp.tsx';
 
 function App() {
 
 
   return (
-    <>
-      <Home />
-    </>
+    <div className='App'>
+      <Router>
+        <Routes>
+          <Route path = '/' element = {<Home />} />
+          <Route path = '/signup' element = {<SignUp />} />
+          <Route path = '*' element = {<Navigate to = "/" />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
