9aa4cd2cf84d85582a0a891d2977b8265b0ed9ea
Joshua Carroll <carrolljosh1976@gmail.com>
2023-11-28 22:34:55 -0500

    Created form for signup page

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -3,7 +3,18 @@ import "./SignUp.css"
 
 function SignUp() {
   return (
-    <div className='signup-background'></div>
+    <div className='signup-background'>
+      <div className='signup-form-container'>
+        <form className='signup-form'>
+          <h2>Sign Up</h2>
+          <label htmlFor = "username">Enter a username</label>
+          <input id = 'username' type = 'text' />
+          <label htmlFor = 'password'>Enter a password</label>
+          <input id = 'password' type = 'password' />
+          <button type = "submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
