89d45f8484dfa6fb79de096a28d7cb3d407f4a70
Joshua Carroll <carrolljosh1976@gmail.com>
2023-12-05 22:26:28 -0500

    added categories for blog posts in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,15 @@ function Navbar() {
   return (
     <div className='navlink-div'>
         <Link className='navlink' to = "/"><h2>Daily Blogs</h2></Link>
+        <ul className='category-list'>
+            <li>General</li>
+            <li>Tech</li>
+            <li>Books</li>
+            <li>Movies & TV</li>
+            <li>Music</li>
+            <li>Gaming</li>
+
+        </ul>
         <ul className='navlink-list'>
             <Link className='navlink' to = "/signup"><li >Sign Up</li></Link>
             <Link className='navlink' to = "/login"><li >Sign In</li></Link>
