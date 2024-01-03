b03374d95727a1feb2720433a78dbac76a931b71
Joshua Carroll <carrolljosh1976@gmail.com>
2024-01-02 19:44:44 -0500

    added headline for banner image div

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,10 @@ import './Home.css';
 function Home() {
   return (
     <div>
-      <div className='banner-image'></div>
+      <div className='banner-image'>
+        <h1>Stay informed.</h1>
+        <h2>Discover stories, advice , and expertise on a variety of topics.</h2>
+      </div>
       <div className='content-div'>
         <div className='blog-posts'></div>
         <div className='author-content'></div>
